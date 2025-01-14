895d90f3212b0685d50cd9b62f67f5aa60a1b073
David Garzaro <garzaro.david@gmail.com>
2025-01-14 16:01:58 -0300

    chore: Implantação da camada IoC

---
+++ src/SalesServiceAPI.IoC/Mapping/MappingProfile.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using SalesServiceAPI.Application.DTOs;
+using SalesServiceAPI.Domain.Entities;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        // Mapeamento de Cliente
+        CreateMap<Cliente, ClienteDTO>().ReverseMap();
+
+        // Mapeamento de ItemVenda
+        CreateMap<ItemVenda, ItemVendaDTO>()
+            .ForMember(dest => dest.ProdutoNome, opt => opt.MapFrom(src => src.Produto.Nome))
+            .ReverseMap();
+
+        // Mapeamento de Produto
+        CreateMap<Produto, ProdutoDTO>().ReverseMap();
+
+        // Mapeamento de Venda
+        CreateMap<Venda, VendaDTO>()
+            .ForMember(dest => dest.ClienteNome, opt => opt.MapFrom(src => src.Cliente.Nome))
+            .ForMember(dest => dest.FilialNome, opt => opt.MapFrom(src => src.Filial.Nome))
+            .ReverseMap();
+
+        // Mapeamento de Filial
+        CreateMap<Filial, FilialDTO>().ReverseMap();
+    }
+}
